d20258d3130bf5768a4c7cc2ee0e8a8d55b2393e
wkyo <wblanksky@hotmail.com>
2024-01-27 11:07:50 +0800

    更新 'auto_replay.sh'

--- auto_replay.sh
+++ auto_replay.sh
@@ -8,12 +8,12 @@ BACKGROUND_PCAP='background.pcap'
 BACKGROUND_REPLAY_SPEED=100
 ATTACK_REPLAY_INTERVAL=15
 ATTACK_REPLAY_DURATION=20
-ATTACK_REPLAY_LOOP=3
+ATTACK_REPLAY_LOOP=1
 ATTACK_REPLAY_SPEED=10
 ATTACK_REPLAY_BULK=true
 ATTACK_PCAPS=()
 DUMP_SNAPLEN=0
-DUMP_PCAP=./dump_output_$(date +'%Y%m%d%H%M%S').pcap
+DUMP_PCAP=./build/dump_output_$(date +'%Y%m%d%H%M%S').pcap
 
 
 function showhelp() {
@@ -25,12 +25,15 @@ Note:
     1. Non-root users need to set network permissions through setcap before using tcpreplay & tcpdump
 
     $ sudo setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' /usr/bin/tcpreplay
+    $ sudo setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' /usr/bin/tcpdump
 
     2. Use virtual ethernet interface for tcpreplay & tcpdump
 
     $ sudo ip link add veth0 type veth peer name veth1
     $ sudo ip link set veth0 up
     $ sudo ip link set veth1 up
+    $ sudo ip link set veth0 mtu 9126
+    $ sudo ip link set veth1 mtu 9126
 "
 }
 
@@ -65,6 +68,10 @@ function main() {
                 shift
                 DUMP_PCAP=${1// /}
                 ;;
+            --loop)
+                shift
+                ATTACK_REPLAY_LOOP=$1
+                ;;
             *)
                 echo "pos $1"
                 ATTACK_PCAPS=("${ATTACK_PCAPS[@]}" "$1")
@@ -82,8 +89,13 @@ function main() {
     BG_DUMP_PID=
     if [ -n "$DUMP_PCAP" ]; then
         echo ">>> Dumping $DUMP_PCAP"
-        # tcpdump -i $OFACE -w $DUMP_PCAP -s $DUMP_SNAPLEN -vv > "${DUMP_PCAP}.log" &
-        tcpdump -i $OFACE -s $DUMP_SNAPLEN -w - | tee ${DUMP_PCAP} | tcpdump -v -r - > "${DUMP_PCAP}.log" &
+        mkdir -p "${DUMP_PCAP%/*}/"
+        if [[ `tcpdump --version | sed -rn 's/tcpdump version\s+([0-9.]+)/\1/p'` > "4.99" ]]; then
+            tcpdump -i $OFACE -s $DUMP_SNAPLEN -w ${DUMP_PCAP} -v --print > "${DUMP_PCAP}.log" &
+        else
+            echo ">> Unsupported tcpdump version"
+            tcpdump -i $OFACE -s $DUMP_SNAPLEN -w - -U | tee ${DUMP_PCAP} | tcpdump -v -r - > "${DUMP_PCAP}.log" &
+        fi
         BG_DUMP_PID=$!
     fi
 
@@ -91,7 +103,7 @@ function main() {
         echo ">>> Bulk-replay is enabled!"
     fi
 
-    for (( i=0; i<=$ATTACK_REPLAY_LOOP; ++i )); do
+    for (( i=0; i<$ATTACK_REPLAY_LOOP; ++i )); do
         echo ">>> Loop $i <<<"
         bulk_pids=()
         for pcapfile in "${ATTACK_PCAPS[@]}"; do
